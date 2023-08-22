0f050ef7dab67d84eb81c92b38dd7c41cb3116c3
Niklas Stiller <sqk6a@Niklass-MacBook-Pro.local>
2023-08-22 09:52:05 +0200

    Added docstrings

--- concatenate.py
+++ concatenate.py
@@ -1,5 +1,5 @@
-
 def concatenate(list_of_strings):
+    """Concatenate a list of strings into a single string."""
     return ' '.join(list_of_strings)
 
 list_of_strings = ['string_1', 'string_2']
