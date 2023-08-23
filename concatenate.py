8516eef53536ff447b731f6dde29aa47484ce48a
Niklas Stiller <sqk6a@Niklass-MacBook-Pro.local>
2023-08-23 15:49:02 +0200

    Check for edge cases

--- concatenate.py
+++ concatenate.py
@@ -1,5 +1,16 @@
 def concatenate(list_of_strings):
     """Concatenate a list of strings into a single string."""
+    
+    # check if list_of_strings contains only strings
+    for string in list_of_strings:
+        if not isinstance(string, str):
+            raise TypeError('list_of_strings must contain only strings!')
+        
+    # check for length of list_of_strings
+    if list_of_strings == []:
+        return ''
+    elif len(list_of_strings) == 1:
+        return list_of_strings[0]
     return ' '.join(list_of_strings)
 
 
--- test_concatenate.py
+++ test_concatenate.py
@@ -3,3 +3,20 @@ from concatenate import concatenate
 def test_concatenate():
     list_of_strings = ['string_1', 'string_2']
     assert 'string_1 string_2' == concatenate(list_of_strings)
+
+def test_concatenate_empty_list():
+    list_of_strings = []
+    assert '' == concatenate(list_of_strings)
+
+def test_concatenate_single_item():
+    list_of_strings = ['string_1']
+    assert 'string_1' == concatenate(list_of_strings)
+
+def test_concatenate_raises_type_error():
+    list_of_strings = ['string_1', 1]
+    try:
+        concatenate(list_of_strings)
+    except TypeError:
+        assert True
+    else:
+        assert False
