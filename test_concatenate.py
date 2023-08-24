1edecfc16e7f3e77a77b8acfe8225f03bd1b8496
niklashtlwrn <stiller.niklas@student.htlwrn.ac.at>
2023-08-24 08:56:43 +0200

    Add tests for edge cases

--- test_concatenate.py
+++ test_concatenate.py
@@ -5,6 +5,14 @@ class TestConcatenate(object):
         list_of_strings = ['string_1', 'string_2']
         assert 'string_1 string_2' == concatenate(list_of_strings)
 
+    def test_concatenate_empty_list(self):
+        list_of_strings = []
+        assert '' == concatenate(list_of_strings)
+
+    def test_concatenate_single_item(self):
+        list_of_strings = ['string_1']
+        assert 'string_1' == concatenate(list_of_strings)
+
     def test_concatenate_raises_type_error(self):
         list_of_strings = ['string_1', 1]
         try:
